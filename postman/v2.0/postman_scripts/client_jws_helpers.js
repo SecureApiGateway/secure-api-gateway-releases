SecureApiGateway/secure-api-gateway-releases

commit c20b434ce96a302f3b35ce5478988c6ce49320e1

1404: Only add openbanking_intent_id claim for OB flow (#62)
Adding this claim for non-OB flows causes issues, as the claim is not supported by the AM OAuth 2.0 Provider.

https://github.com/SecureApiGateway/SecureApiGateway/issues/1404

postman/v2.0/postman_scripts/client_jws_helpers.js
@@ -140,10 +140,6 @@ client_jws_helpers.createAuthorizeJwtData = function(scope, consentId, jarm) {
               "acr": {
               "value": acr,
               "essential": true
-            },
-            "openbanking_intent_id": {
-              "value": consentId,
-              "essential": true
             }
           }
         },
@@ -152,6 +148,13 @@ client_jws_helpers.createAuthorizeJwtData = function(scope, consentId, jarm) {
         "state": "10d260bf-a7d9-444a-92d9-7b7a5f088208",
         "nonce": "10d260bf-a7d9-444a-92d9-7b7a5f088208",
         "client_id": pm.environment.get("client_id")
+    }
+
+    if (acr.includes("openbanking")) {
+        data.claims["openbanking_intent_id"] = {
+            "value": consentId,
+            "essential": true
+        }
     }
 
     if (jarm) {

commit b40a99f11c8318f300af50eea2b97c5d25df98e0

1404: Set openbanking_intent_id claim within id_token claim
Fixes a bug introduced by PR: https://github.com/SecureApiGateway/secure-api-gateway-releases/pull/62

The openbanking_intent_id claim need to be set as a field within claims.id_token.

https://github.com/SecureApiGateway/SecureApiGateway/issues/1404

postman/v2.0/postman_scripts/client_jws_helpers.js
@@ -130,18 +130,18 @@ client_jws_helpers.createAuthorizeJwtData = function(scope, consentId, jarm) {
     let acr = pm.environment.has("AUTHORIZE_REQUEST_ACR") ? pm.environment.get("AUTHORIZE_REQUEST_ACR") : "urn:openbanking:psd2:ca"
 
     var data = {
-          "aud": audience,
-          "scope": scope,
-          "iss": pm.environment.get("client_id"),
-          "exp": exp,
-          "nbf": nbf,
-          "claims": {
+        "aud": audience,
+        "scope": scope,
+        "iss": pm.environment.get("client_id"),
+        "exp": exp,
+        "nbf": nbf,
+        "claims": {
             "id_token": {
-              "acr": {
-              "value": acr,
-              "essential": true
+                "acr": {
+                    "value": acr,
+                    "essential": true
+                }
             }
-          }
         },
         "response_type": "code id_token",
         "redirect_uri": pm.environment.get("client_redirect_uri"),
@@ -151,7 +151,7 @@ client_jws_helpers.createAuthorizeJwtData = function(scope, consentId, jarm) {
     }
 
     if (acr.includes("openbanking")) {
-        data.claims["openbanking_intent_id"] = {
+        data.claims.id_token["openbanking_intent_id"] = {
             "value": consentId,
             "essential": true
         }